c3966ef61f753e399db1756f0c55580ce7c5f9f8
Dayeon Yu <0p25dy@naver.com>
2023-05-05 18:59:48 +0900

    [fix] fix

--- src/main/java/com/feedback/hafit/controller/CategoryController.java
+++ src/main/java/com/feedback/hafit/controller/CategoryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Category")
+@RequestMapping("/category")
 @RequiredArgsConstructor
 public class CategoryController {
 
