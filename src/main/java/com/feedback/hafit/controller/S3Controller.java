2c6e1a719c11d3ea1c1883d4d5e289474f403c3f
Dayeon Yu <0p25dy@naver.com>
2023-06-02 13:40:40 +0900

    [fix] S3 하는 중...

--- src/main/java/com/feedback/hafit/controller/S3Controller.java
+++ src/main/java/com/feedback/hafit/controller/S3Controller.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/upload")
@@ -15,4 +18,10 @@ public class S3Controller {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    @PostMapping("/upload")
+    public String upload(@RequestPart("file") MultipartFile file) {
+        // 업로드된 파일을 S3에 저장하고 파일 URL을 반환
+        String fileUrl = s3Service.upload(file, bucket);
+        return fileUrl;
+    }
 }
