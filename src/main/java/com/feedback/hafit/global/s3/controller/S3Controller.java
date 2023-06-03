77cc54e2866e5aa04b31bd0c6a7749747ab53c3b
JongHun <103156018+WDJJH@users.noreply.github.com>
2023-06-03 21:52:54 +0900

    [fix] s3 upload

--- src/main/java/com/feedback/hafit/global/s3/controller/S3Controller.java
+++ src/main/java/com/feedback/hafit/global/s3/controller/S3Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/upload")
+@RequestMapping("/file")
 @RequiredArgsConstructor
 public class S3Controller {
     private final S3Service s3Service;
