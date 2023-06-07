584f4fefa43921d00058277e5fb85db3e5ce0438
ooyniz <ooyniz@gmail.com>
2023-06-07 20:27:05 +0900

    [fix] 피드 등록에 user Email 값 추가, 수정 완료, security cors 추가

--- src/main/java/com/feedback/hafit/domain/post/dto/response/PostDTO.java
+++ src/main/java/com/feedback/hafit/domain/post/dto/response/PostDTO.java
@@ -1,31 +1,48 @@
 package com.feedback.hafit.domain.post.dto.response;
 
 import com.feedback.hafit.domain.BaseEntity;
+import com.feedback.hafit.domain.category.entity.Category;
+import com.feedback.hafit.domain.post.entity.FileImage;
 import com.feedback.hafit.domain.post.entity.Post;
+import com.feedback.hafit.domain.user.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostDTO extends BaseEntity {
     private Long postId;
     private Long categoryId;
-    private Long userId;
+    private String userEmail;
     private String postContent;
     private List<FileImageDTO> files = Collections.emptyList();
 
     public PostDTO(Post post, List<FileImageDTO> files) {
         this.postId = post.getPostId();
         this.categoryId = post.getCategory().getCategoryId();
-        this.userId = post.getUser().getUserId();
+        this.userEmail = post.getUser().getEmail();
         this.postContent = post.getPostContent();
         this.files = files;
         this.modifiedAt = post.getModifiedAt();
         this.createdAt = post.getCreatedAt();
+    }
+
+    public PostDTO(Long postId, List<FileImage> fileImages, String postContent, Category category, User user) {
+        this.postId = postId;
+        this.categoryId = category.getCategoryId();
+        this.userEmail = user.getEmail();
+        this.postContent = postContent;
+        // Convert the list of FileImage objects to FileImageDTO objects
+        this.files = fileImages.stream()
+                .map(FileImageDTO::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/feedback/hafit/domain/post/service/PostService.java
+++ src/main/java/com/feedback/hafit/domain/post/service/PostService.java
@@ -12,45 +12,43 @@ import com.feedback.hafit.domain.post.entity.Post;
 import com.feedback.hafit.domain.post.repository.FileImageRepository;
 import com.feedback.hafit.domain.post.repository.PostRepository;
 import com.feedback.hafit.domain.user.entity.User;
+import com.feedback.hafit.domain.user.repository.UserRepository;
 import com.feedback.hafit.domain.user.service.UserService;
 import com.feedback.hafit.global.s3.service.S3Service;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PostService {
+    private final UserRepository userRepository;
+    private final UserService userService;
 
-    @Autowired
-    UserService userService;
+    private final PostRepository postRepository;
 
-    @Autowired
-    PostRepository postRepository;
+    private final CategoryRepository categoryRepository;
 
-    @Autowired
-    CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
 
-    @Autowired
-    CategoryService categoryService;
+    private final S3Service s3Service;
 
-    @Autowired
-    S3Service s3Service;
-
-    @Autowired
-    FileImageRepository fileImageRepository;
+    private final FileImageRepository fileImageRepository;
 
     @Transactional
-    public PostDTO upload(PostCreateDTO postDTO) {
-        Long userId = postDTO.getUserId(); // Long userId
-        Long categoryId = postDTO.getCategoryId(); // Long categoryId
-        User user = userService.getById(userId);
+    public PostDTO upload(PostCreateDTO postDTO, List<MultipartFile> files, String email) {
+        Long categoryId = postDTO.getCategoryId();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with email: " + email));
         Category category = categoryService.getById(categoryId);
         String postContent = postDTO.getPostContent();
         Post post = postRepository.save(Post.builder()
@@ -60,26 +58,46 @@ public class PostService {
                 .build()
         );
 
-        List<FileImageDTO> files = postDTO.getFiles().stream().map(file -> {
+        List<FileImageDTO> fileImageDTOs = new ArrayList<>();
+        for (MultipartFile file : files) {
             String uploadUrl = s3Service.upload(file, "posts");
             log.info("uploadUrl: {}", uploadUrl);
-            return new FileImageDTO(fileImageRepository.save(
+            FileImageDTO fileImageDTO = new FileImageDTO(fileImageRepository.save(
                     FileImage.builder()
                             .post(post)
                             .fileName(uploadUrl)
                             .build()
             ));
-        }).toList();
-        return new PostDTO(post, files);
+            fileImageDTOs.add(fileImageDTO);
+        }
+
+        return new PostDTO(post, fileImageDTOs);
     }
 
-
-    public boolean update(PostUpdateDTO postDTO) {
+    public boolean update(Long postId, PostUpdateDTO postDTO, List<MultipartFile> files) {
         try {
-            Optional<Post> optionalPost = postRepository.findById(postDTO.getPostId());
+            Optional<Post> optionalPost = postRepository.findById(postId);
             if (optionalPost.isPresent()) {
                 Post post = optionalPost.get();
                 post.setPostContent(postDTO.getPostContent());
+
+                Long categoryId = postDTO.getCategoryId();
+                Category category = categoryService.getById(categoryId);
+                post.setCategory(category);
+
+                List<FileImageDTO> fileImageDTOs = new ArrayList<>();
+                for (MultipartFile file : files) {
+                    String uploadUrl = s3Service.upload(file, "posts");
+                    log.info("uploadUrl: {}", uploadUrl);
+                    FileImageDTO fileImageDTO = new FileImageDTO(fileImageRepository.save(
+                            FileImage.builder()
+                                    .post(post)
+                                    .fileName(uploadUrl)
+                                    .build()
+                    ));
+                    fileImageDTOs.add(fileImageDTO);
+                }
+
                 postRepository.save(post);
                 return true;
             } else {
