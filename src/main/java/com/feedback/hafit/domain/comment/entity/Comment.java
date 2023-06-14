876f032c5e1cdb3d3ab2afd094d8953b6e44744a
ooyniz <ooyniz@gmail.com>
2023-06-14 19:03:08 +0900

    [fix] 엔티티 수정

--- src/main/java/com/feedback/hafit/domain/comment/entity/Comment.java
+++ src/main/java/com/feedback/hafit/domain/comment/entity/Comment.java
@@ -6,12 +6,9 @@ import com.feedback.hafit.domain.user.entity.User;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "comments")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -26,15 +23,6 @@ public class Comment extends BaseEntity {
     @Column(name = "content", nullable = false)
     private String content; // 댓글 내용
 
-    @ManyToMany
-    @JoinTable(
-            name = "comment_likes",
-            joinColumns = @JoinColumn(name = "comment_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id")
-    )
-
-    private Set<User> likedUsers = new HashSet<>(); // 좋아요를 누른 사용자들
-
     @ManyToOne(fetch = FetchType.LAZY) // user ID
     @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_comment_user"))
     private User user;
@@ -43,23 +31,4 @@ public class Comment extends BaseEntity {
     @JoinColumn(name = "post_id", foreignKey = @ForeignKey(name = "fk_comment_post"))
     private Post post;
 
-
-    public Comment(User user, String content, Post post) {
-        this.user = user;
-        this.post = post;
-        this.content = content;
-    }
-
-    // 댓글 좋아요 로직
-    public void addLike(User user) {
-        likedUsers.add(user);
-    }
-
-    public void removeLike(User user) {
-        likedUsers.remove(user);
-    }
-
-    public int getLikesCount() {
-        return likedUsers.size();
-    }
 }
