90b21076cf7820eb774109abd8682e0ac27d87b4
Dayeon Yu <0p25dy@naver.com>
2023-06-19 19:07:29 +0900

    [add] EntityNotFoundExceptio 처리

--- src/main/java/com/feedback/hafit/global/exception/ApiExceptionAdvice.java
+++ src/main/java/com/feedback/hafit/global/exception/ApiExceptionAdvice.java
@@ -6,16 +6,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.persistence.EntityNotFoundException;
+
 @RestControllerAdvice
 @Slf4j
 public class ApiExceptionAdvice {
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(
+            EntityNotFoundException e
+    ) {
+        log.error("EntityNotFoundException 발생!");
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> IllegalArgumentException(
             IllegalArgumentException e
-    ){
+    ) {
         log.error("illegal args exception 발생!");
         e.printStackTrace();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
+
+
 }
