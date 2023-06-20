a1ff33fe4c7656146bb2cfd6f3a698b8d7d91631
ooyniz <ooyniz@gmail.com>
2023-06-20 16:20:43 +0900

    [!BREAKING CHANGE] comment-like, post-like 테이블 return 값 변경

--- src/main/java/com/feedback/hafit/domain/commentLike/controller/CommentLikeController.java
+++ src/main/java/com/feedback/hafit/domain/commentLike/controller/CommentLikeController.java
@@ -3,7 +3,6 @@ package com.feedback.hafit.domain.commentLike.controller;
 import com.feedback.hafit.domain.commentLike.service.CommentLikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -17,14 +16,12 @@ public class CommentLikeController {
     private final CommentLikeService commentLikeService;
 
     @PostMapping("/{commentId}")
-    public ResponseEntity<Boolean> insertCommentLike(@PathVariable Long commentId, Principal principal) throws Exception  {
+    public void insertCommentLike(@PathVariable Long commentId, Principal principal) throws Exception  {
         commentLikeService.insertCommentLike(commentId, principal.getName());
-        return ResponseEntity.ok(true);
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<Boolean> deleteCommentLike(@PathVariable Long commentId, Principal principal) {
+    public void deleteCommentLike(@PathVariable Long commentId, Principal principal) {
         commentLikeService.deleteCommentLike(commentId, principal.getName());
-        return ResponseEntity.ok(true);
     }
 }
--- src/main/java/com/feedback/hafit/domain/postLike/controller/PostLikeController.java
+++ src/main/java/com/feedback/hafit/domain/postLike/controller/PostLikeController.java
@@ -3,7 +3,6 @@ package com.feedback.hafit.domain.postLike.controller;
 import com.feedback.hafit.domain.postLike.service.PostLikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -17,15 +16,13 @@ public class PostLikeController {
     private final PostLikeService postLikeService;
 
     @PostMapping("/{postId}")
-    public ResponseEntity<Boolean> insertPostLike(@PathVariable Long postId, Principal principal) {
+    public void insertPostLike(@PathVariable Long postId, Principal principal) {
         postLikeService.insertPostLike(postId, principal.getName());
-        return ResponseEntity.ok(true);
     }
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Boolean> deletePostLike(@PathVariable Long postId, Principal principal) {
+    public void deletePostLike(@PathVariable Long postId, Principal principal) {
         postLikeService.deletePostLike(postId, principal.getName());
-        return ResponseEntity.ok(true);
     }
 
 }
